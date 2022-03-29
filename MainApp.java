64d60cf2bac80dfdc934f5a438f7afff9b4daae0
your name <abc@gmail.com>
2022-03-29 09:35:06 +0700

    first commit

---
+++ MainApp.java
@@ -0,0 +1,53 @@
+package basic.dev;
+/**
+ * 
+ * @author PhanTrongHieu
+ *
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		/*
+		 * Thông Tin Sinh Viên
+		 */
+		String Ten = "Phan Trong Hieu";
+		int MaSinhVien = 160920011;
+		int CMND = 191917211;
+		String TenLop = "Lap Trinh Web Java";
+		String TenTruong = "Hue CIT";
+		float DiemTrungBinh = 8.7f;
+		
+		System.out.println("Ten: "+ Ten);
+		System.out.println("Ma Sinh Vien: "+ MaSinhVien);
+		System.out.println("CMND: "+ CMND);
+		System.out.println("Ten Lop: "+ TenLop);
+		System.out.println("Ten Truong: "+ TenTruong);
+		System.out.println("Diem Trung Binh: "+ DiemTrungBinh);
+		/**
+		 * Thông tin công nhân
+		 */
+		String ten = " Phan Trong Hieu ";
+		String DiaChi = "Nguyen Cong Tru, Phuong Phu Hoi, TP.Hue";
+		float SDT = 0932428163f;
+		int HeSoLuong = 8;
+		
+		System.out.println("Ten: "+ Ten);
+		System.out.println("Dia Chi: "+ DiaChi);
+		System.out.println("SDT: "+ SDT);
+		System.out.println("He So Luong: "+ HeSoLuong);
+		/**
+		 * Thông tin một chiếc xe car
+		 */
+		String TenXe = "Ducati Motor Holding S.p.A.";
+		String Hang = "Ducati Corse";
+		float Gia = 4.5f;
+		String MauSon = "Black";
+		
+		System.out.println("Ten Xe: "+ TenXe);
+		System.out.println("Hang: "+ Hang);
+		System.out.println("Gia: "+ Gia);
+		System.out.println("Mau Son: "+ MauSon);
+		
+		
+	}
+
+}
